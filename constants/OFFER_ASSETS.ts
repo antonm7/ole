0fa634b5dfa0a68ef107a5c6bfdb57fd62dc8b3a
Anton <migolkoanton@gmail.com>
2025-10-09 11:51:18 +0300

    Update offers in HomePage with new stadium experience and discounts; add images for both clubs and enhance offer descriptions for clarity and user engagement.

--- constants/OFFER_ASSETS.ts
+++ constants/OFFER_ASSETS.ts
@@ -13,12 +13,10 @@ export const CLUB_LOGOS: Record<ClubKey, any> = {
       ticket: any;
       sponsor1: any;
       sponsor2: any;
-      homeA?: any;
-      homeB?: any;
-      homeC?: any;
       shirt2?: any;
       shirt3?: any;
       shirt4?: any;
+    stadium?:any;
     }
   > = {
     "hapoel-tel-aviv": {
@@ -27,13 +25,11 @@ export const CLUB_LOGOS: Record<ClubKey, any> = {
       ticket: require("../assets/offers/hapoel-tel-aviv/logo.png"),
       sponsor1: require("../assets/offers/hapoel-tel-aviv/sponser1.jpg"),
       sponsor2: require("../assets/offers/hapoel-tel-aviv/sponser2.webp"),
-      // Optional: replace with real mockups if you have them
-      homeA: require("../assets/offers/hapoel-tel-aviv/shirt.jpg"),
-      homeB: require("../assets/offers/hapoel-tel-aviv/shirt.jpg"),
-      homeC: require("../assets/offers/hapoel-tel-aviv/shirt.jpg"),
       shirt2: require("../assets/offers/hapoel-tel-aviv/shirt2.jpeg"),
       shirt3: require("../assets/offers/hapoel-tel-aviv/shirt3.jpeg"),
       shirt4: require("../assets/offers/hapoel-tel-aviv/shirt4.jpeg"),
+      stadium:require("../assets/offers/hapoel-tel-aviv/stadium.jpg"),
+
     },
     "maccabi-haifa": {
       shirt: require("../assets/offers/maccabi-haifa/shirt.jpg"),
@@ -44,8 +40,6 @@ export const CLUB_LOGOS: Record<ClubKey, any> = {
       ticket: require("../assets/offers/maccabi-haifa/logo.png"),
       sponsor1: require("../assets/offers/maccabi-haifa/sponser1.png"),
       sponsor2: require("../assets/offers/maccabi-haifa/sponser2.png"),
-      homeA: require("../assets/offers/maccabi-haifa/shirt.jpg"),
-      homeB: require("../assets/offers/maccabi-haifa/shirt.jpg"),
-      homeC: require("../assets/offers/maccabi-haifa/shirt.jpg"),
+      stadium:require("../assets/offers/maccabi-haifa/stadium.jpg"),
     },
   };
